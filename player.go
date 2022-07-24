85515665271efa0e906b7de8526d5d352869b438
Anıl Konaç <anil.konac@gmail.com>
2022-07-24 22:42:23 +0300

    Draw player

--- player.go
+++ player.go
@@ -7,16 +7,21 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const (
+	breadthSpeedMax = 1.5
+	breadthSpeedMin = 0.5
+)
+
 var (
 	timePassedSec float32
 	playerShader  *ebiten.Shader
 	//go:embed player.kage.go
-	shaderByte []byte
+	shaderBytes []byte
 )
 
 func init() {
 	var err error
-	playerShader, err = ebiten.NewShader(shaderByte)
+	playerShader, err = ebiten.NewShader(shaderBytes)
 	if err != nil {
 		panic(err)
 	}
@@ -35,9 +40,10 @@ func newPlayer(posX, posY uint8, color color.RGBA) *player {
 		RGBA: color,
 		drawOpts: ebiten.DrawRectShaderOptions{
 			Uniforms: map[string]interface{}{
-				"TileRadius": float32(tileLength / 2.0),
-				"Time":       float32(timePassedSec),
-				"Color":      []float32{float32(color.R), float32(color.G), float32(color.B), float32(color.A)},
+				"TileRadius":  float32(tileLength / 2.0),
+				"Time":        float32(timePassedSec),
+				"BreathSpeed": float32(breadthSpeedMax),
+				"Color":       []float32{float32(color.R), float32(color.G), float32(color.B), float32(color.A)},
 			},
 		},
 	}
--- player.kage.go
+++ player.kage.go
@@ -2,16 +2,20 @@
 
 package main
 
-const pi = 3.14159
+const (
+	pi          = 3.14159
+	breathRatio = 1.0 / 5.0
+)
 
 var (
-	TileRadius float
-	Time       float
-	Color      vec4
+	TileRadius  float
+	Time        float
+	BreathSpeed float
+	Color       vec4
 )
 
 func Fragment(pos vec4, tex vec2, col vec4) vec4 {
-	radius := TileRadius / 2.0 * (1.0 + sin(Time*pi))
+	radius := TileRadius * ((1.0 - breathRatio) + breathRatio*sin(BreathSpeed*Time*pi))
 	if distance(tex, vec2(TileRadius)) >= radius {
 		return vec4(0.0)
 	}
